e4b4f60b175d481b1d7b62e6d06a3ef557d81c6e
Muhd Jamil Husni <maintanance1991@gmail.com>
2022-08-06 21:52:44 +0800

    Update index.php
    first version

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 ?>
 <html>
+    <style>
+    .textcolumn {
+        border-radius:10px;
+        height : 35px;
+        width : 200px;
+        margin : 5px;
+    }
+    </style>
+    <center>
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+        <h1>Login</h1>    
+        <input class="textcolumn" type="text" name="username" id="username"/> </br>
+        <input class="textcolumn" type="password" name="password" id="password"/>
+        </form>
+    </center>
 </html>
