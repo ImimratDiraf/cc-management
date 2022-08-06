0a528eb874d750262bdc4dc3132f89de10f36d62
ImimratDiraf <tarmimif28@gmail.com>
2022-08-06 23:20:29 +0800

    MIL aku dah ubah

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
-<html>
+<html lang="en">
     <head>
-        <link rel="stylesheet" href="./css/style.css">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/style.css">
+
     </head>
-    <center>
-    <div class="">
-        <form action="./login_act.php" method="POST">
-            <h1>Login</h1>    
-            Username : <input class="textcolumn" type="text" name="username" id="username"/> </br>
-            Password : <input class="textcolumn" type="password" name="password" id="password"/> </br>
-            <input style="width:270px;" class="textcolumn" type="submit" value="Log in"/>
-        </form>
-    </div>
-    </center>
+    <body>
+        <section class="container forms">
+            <div class="form login">
+                <div class="form-content">
+                    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+                        <h1>Login</h1>
+
+                        <div class="field input-field">   
+                            Username : <input class="textcolumn" type="text" name="username" id="username"/> </br>
+                        </div>
+
+                        <div class="field input-field">
+                        Password : <input class="textcolumn" type="password" name="password" id="password"/> </br>
+                        </div>
+                        
+                        <div class="field button-field">
+                        <input style="width:270px;" class="textcolumn" type="submit" value="Log in"/>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </section>
+    </body>
 </html>
